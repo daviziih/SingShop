f161ac9fedd4acfa2b950d71884918b917b9dcc7
Davi Gabriel <Davi Gabriel@PC-PANTALON>
2020-10-02 00:32:51 -0300

    update 01/10/2020

--- me/davi/listeners/PlayerJoinEvent.java
+++ me/davi/listeners/PlayerJoinEvent.java
@@ -4,13 +4,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
-import me.davi.api.Mensagens;
-
 public final class PlayerJoinEvent implements Listener {
 
 	@EventHandler(ignoreCancelled = true)
 	private void onPreLogin(PlayerLoginEvent e) {
-		if (e.getPlayer().getName().equalsIgnoreCase(Mensagens.nomeLoja)) {
+		if (e.getPlayer().getName().equalsIgnoreCase("Loja")) {
 			e.setKickMessage("&cVocê não pode entrar no servidor com o mesmo nome da loja");
 			e.disallow(PlayerLoginEvent.Result.KICK_OTHER, e.getKickMessage());
 		}
